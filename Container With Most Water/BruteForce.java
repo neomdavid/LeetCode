44c5087e06eb392ff8b208758d61f2b71e88b243
neomdavid <neomanaogdavid@gmail.com>
2024-03-22 15:57:01 +0800

    Container With Most Water- Brute Force

---
+++ Container With Most Water/BruteForce.java
@@ -0,0 +1,14 @@
+
+class Solution {
+  public int maxArea(int[] height) {
+      int result = 0;
+      
+      for(int l = 0; l <height.length; l++){
+          for (int r = l+1; r<height.length; r++){
+              int area = (r-l)*Math.min(height[r], height[l]);
+              result = Math.max(area,result);
+          }
+      }
+      return result;
+  }
+}
