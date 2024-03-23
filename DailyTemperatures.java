b673ed7e87f9ee75a8cc174565dc20655c88ae76
neomdavid <neomanaogdavid@gmail.com>
2024-03-23 22:52:00 +0800

    Daily Temperatures - Stack

---
+++ DailyTemperatures.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int[] dailyTemperatures(int[] temperatures) {
+      int result [] = new int [temperatures.length];
+      Stack <Integer> stack = new Stack();
+
+      for(int i = 0; i < temperatures.length; i++){
+          while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+              int element = stack.pop();
+              result[element] = i - element;
+          }
+          stack.push(i);
+      }
+      return result;
+  }
+}
